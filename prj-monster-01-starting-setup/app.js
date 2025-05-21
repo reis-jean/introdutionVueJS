82f41c780d5bbd0ca285bebe4ecd01d9718b4bc0
Jean Reis <jvictor4919@gmail.com>
2025-05-21 05:28:08 -0300

    special atack, heal

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -37,6 +37,25 @@ const app = Vue.createApp({
             const attackValue = getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
+        },
+        watch:{
+            playerHealth(value){
+                if(value<= 0 && this.monsterHealth <=0 ){
+                    // A draw
+                }else if (value <= 0){
+                     
+                }
+            }
+        },
+        healPlayer(){
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if(this.player + healValue> 100){
+                this.playerHealth = 100;
+            }else{
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
         }
 
     }
