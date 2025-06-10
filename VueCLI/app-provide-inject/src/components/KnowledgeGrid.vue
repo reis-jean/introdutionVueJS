1032a517a9bdd2fc419f475cd50e3305436f08fa
Jean Reis <jvictor4919@gmail.com>
2025-06-10 07:09:04 -0300

    Feat: introducion provider

---
+++ VueCLI/app-provide-inject/src/components/KnowledgeGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  //este component filho herda o provider pois o base esta herda de App (vue principal), 
+  inject: ['topics'],
+  emits: ['select-topic']
+};
+</script>
