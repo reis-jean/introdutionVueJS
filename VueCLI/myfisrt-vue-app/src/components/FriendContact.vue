f3be0349530af205e8e6c1e00ca1e1351904f334
Jean Reis <jvictor4919@gmail.com>
2025-06-09 22:21:43 -0300

    defining & validating events

--- VueCLI/myfisrt-vue-app/src/components/FriendContact.vue
+++ VueCLI/myfisrt-vue-app/src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : ''}}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : ''}}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
@@ -20,6 +20,10 @@
 export default {
   // props: ['name', 'phoneNumber', 'emailAddress', 'isFavorite'],
   props: {
+    id:{
+      type: String,
+      required: true
+    },
     name: {
       type: String,
       required: true,
@@ -41,10 +45,10 @@ export default {
       // }
     },
   },
+  emits: ['toggle-favorite'],
   data() {
     return {
-      detailsAreVisible: false,
-      friendIsFavorite: this.isFavorite,
+      detailsAreVisible: false
     };
   },
   methods: {
@@ -52,7 +56,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorite() {
-      this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit('toggle-favorite', this.id );
     },
   },
 };
